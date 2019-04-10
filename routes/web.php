874ff20d551a7285a1156c6ca7e31671f2cf47be
Weilun Liu <763027562@qq.com>
2019-04-10 22:21:51 +0000

    initializing the user account coding

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,29 @@
 //     return view('welcome');
 // });
 
+// Route::get('/', 'PagesController@root')->name('root');
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', 'PagesController@root')->name('root');
+
+// authentication corresponded function
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// password resetting
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Email validation
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
