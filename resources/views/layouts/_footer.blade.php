667c0023c0275aa4051cfe092bc80e619dd90779
Weilun Liu <763027562@qq.com>
2019-04-10 02:14:25 +0000

    basic page construct

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      <a href="#" target="_blank">Team-Mint</a> Design and Maintain <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:763027562@qq.com">Contact us</a></p>
+  </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home page')
+
+@section('content')
+  <h1>this is home page</h1>
+@stop
