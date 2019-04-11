5906bc8fdc4097e0bb7139c3a65186caea6cafac
Weilun Liu <763027562@qq.com>
2019-04-11 03:04:14 +0000

    modify personnel info

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <div class="col-md-8 offset-md-2">
+
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          <i class="glyphicon glyphicon-edit"></i> Modify Personnal Info
+        </h4>
+      </div>
+
+      <div class="card-body">
+
+        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
+          <input type="hidden" name="_method" value="PUT">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          
+           @include('shared._error')
+
+          <div class="form-group">
+            <label for="name-field">User Name</label>
+            <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
+          </div>
+          <div class="form-group">
+            <label for="email-field">Email</label>
+            <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
+          </div>
+          <div class="form-group">
+            <label for="introduction-field">Personnal Description</label>
+            <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+          </div>
+          <div class="well well-sm">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
