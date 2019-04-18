ee0287209c9895066da081c35d3bfb30c543d9f8
Weilun Liu <763027562@qq.com>
2019-04-18 00:23:20 +0000

    topic post content page display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield('title', 'LaraBBS') - Laravel powered</title>
+  <meta name="description" content="@yield('description', 'LaraBBS fnas club')" />
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
